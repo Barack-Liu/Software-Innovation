499d019e56c2807134cacde7720bea04061afaba
dpls <dumplings_ming@126.com>
2018-06-27 23:06:27 +0800

    use fg deploy task for test usage

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo "-------begin deploy----------"
-sshpass -p "dplsPublic12" ssh -o StrictHostKeyChecking=no zzm@218.193.183.164 'bash -s' < remote-deploy.sh &
+sshpass -p "dplsPublic12" ssh -o StrictHostKeyChecking=no zzm@218.193.183.164 'bash -s' < remote-deploy.sh
 echo "-------finish deploy----------"
