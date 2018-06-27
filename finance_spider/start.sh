30bdfc3e9d50e6c1afa265da512b8db958e1ebc0
dpls <dumplings_ming@126.com>
2018-06-27 16:01:26 +0800

    removed result.json from git and modified spider start script to catch all data concurrently

--- finance_spider/start.sh
+++ finance_spider/start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-#scrapy crawl eastmoney -o result.json >/dev/null 2>&1 &
-#scrapy crawl 10jqka -o result.json >/dev/null 2>&1 &
-#scrapy crawl qq -o result.json >/dev/null 2>&1 &
-#scrapy crawl sina -o result.json >/dev/null 2>&1 &
-#scrapy crawl cnstock -o result.json >/dev/null 2>&1 &
-#scrapy crawl sohu -o result.json >/dev/null 2>&1 &
-scrapy crawl 10jqka -o result.json
+scrapy crawl eastmoney -o result.json >/dev/null 2>&1 &
+scrapy crawl 10jqka -o result.json >/dev/null 2>&1 &
+scrapy crawl qq -o result.json >/dev/null 2>&1 &
+scrapy crawl sina -o result.json >/dev/null 2>&1 &
+scrapy crawl cnstock -o result.json >/dev/null 2>&1 &
+scrapy crawl sohu -o result.json >/dev/null 2>&1 &
