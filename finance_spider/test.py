7f64ed574da08b0b480cdf3fb5032506b60c9c43
BarackO <yanghongliu2013@outlook.com>
2018-06-27 16:54:53 +0800

    unit test

---
+++ finance_spider/test.py
@@ -0,0 +1,15 @@
+import unittest
+import finance
+
+class TestSpider(unittest.TestCase):
+	"""docstring for TestSpider"""
+	def __init__(self, arg):
+		super(TestSpider, self).__init__()
+		self.arg = arg
+	
+	def test_json(self):
+		self.assertTrue(f=open(result.json))
+			
+
+if __name__ == '__main__':
+	unittest.main()
