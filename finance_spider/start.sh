78433afea0fceeb10079b623b5bb6ec99c6e01ab
dpls <dumplings_ming@126.com>
2018-06-27 12:41:01 +0800

    added back bin and modified travis.yml to test if scrapy exists

--- finance_spider/start.sh
+++ finance_spider/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-scrapy crawl eastmoney -o result.json >/dev/null 2>&1 &
-scrapy crawl 10jqka -o result.json >/dev/null 2>&1 &
-scrapy crawl qq -o result.json >/dev/null 2>&1 &
-scrapy crawl sina -o result.json >/dev/null 2>&1 &
-scrapy crawl cnstock -o result.json >/dev/null 2>&1 &
-scrapy crawl sohu -o result.json >/dev/null 2>&1 &
+#scrapy crawl eastmoney -o result.json >/dev/null 2>&1 &
+#scrapy crawl 10jqka -o result.json >/dev/null 2>&1 &
+#scrapy crawl qq -o result.json >/dev/null 2>&1 &
+#scrapy crawl sina -o result.json >/dev/null 2>&1 &
+#scrapy crawl cnstock -o result.json >/dev/null 2>&1 &
+#scrapy crawl sohu -o result.json >/dev/null 2>&1 &
+scrapy crawl 10jqka -o result.json
