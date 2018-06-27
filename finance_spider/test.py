d75d947676d0ab70b6b8f25dbff423330515591b
BarackO <yanghongliu2013@outlook.com>
2018-06-27 17:13:39 +0800

    test

--- finance_spider/test.py
+++ finance_spider/test.py
@@ -3,9 +3,9 @@ import finance
 
 class TestSpider(unittest.TestCase):
 	"""docstring for TestSpider"""
-	def __init__(self, arg):
-		super(TestSpider, self).__init__()
-		self.arg = arg
+	# def __init__(self, arg):
+	# 	super(TestSpider, self).__init__()
+	# 	self.arg = arg
 	
 	def test_json(self):
 		self.assertTrue(f=open(result.json))
